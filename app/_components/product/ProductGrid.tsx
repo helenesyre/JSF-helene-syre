2e483ae2ebbf4e4ffa2856c2e4296ab028916997
Helene Syre <syrehelene@gmail.com>
2026-09-21 22:15:23 +0200

    Update ProductGrid.tsx

--- app/_components/product/ProductGrid.tsx
+++ app/_components/product/ProductGrid.tsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 export default function ProductGrid({ products }: { products: Product[] }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
       {products.length > 0
         ? products.map((product, index) => {
             return (
