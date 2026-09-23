6dc686c3c643a41bef389c89907e459adb828a39
Helene Syre <syrehelene@gmail.com>
2026-09-23 21:57:52 +0200

    Update Breadcrumbs.tsx
    Added color to my breadcrumb, and placed it better on the page

--- app/_components/ui/Breadcrumbs.tsx
+++ app/_components/ui/Breadcrumbs.tsx
@@ -2,18 +2,27 @@ import { BreadCrumb } from "@/app/_lib/types";
 import Link from "next/link";
 
 export default function BreadCrumbs({ crumbs }: { crumbs: BreadCrumb[] }) {
+  const breadcrumbColor = "text-stone-900";
+
   return (
-    <nav aria-label="Breadcrumb" className="relative flex mb-4 font-medium">
+    <nav
+      aria-label="Breadcrumb"
+      className="relative flex mb-4 font-medium top-12 left-12"
+    >
       {crumbs.length > 0 &&
         crumbs.map((crumb, index) => {
           return (
             <span key={index} className="flex">
               {crumb.link ? (
-                <Link href={crumb.link}>{crumb.label}</Link>
+                <Link href={crumb.link} className={breadcrumbColor}>
+                  {crumb.label}
+                </Link>
               ) : (
-                crumb.label
+                <span className={breadcrumbColor}>{crumb.label}</span>
               )}
-              {index + 1 < crumbs.length && <span className="mx-2">›</span>}
+              {index + 1 < crumbs.length && (
+                <span className={`mx-2 ${breadcrumbColor}`}>›</span>
+              )}
             </span>
           );
         })}
