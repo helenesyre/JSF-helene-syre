df4bae802402824be99a73bfe23b7d84a5408d37
Helene Syre <syrehelene@gmail.com>
2026-09-24 17:12:40 +0200

    Linking the shopping cart process

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,17 @@
+import Button from "../_components/ui/Button";
+
 export default function Cart() {
   return (
-    <div className="py-8">
+    <div className="py-8 px-12">
       <h1>Cart page</h1>
+      <Button
+        variant={"Medium"}
+        color={"Primary"}
+        width={"Fit"}
+        href="/cart/payment"
+      >
+        Proceed to Payment
+      </Button>
     </div>
   );
 }
--- app/cart/success/page.tsx
+++ app/cart/success/page.tsx
@@ -1,7 +1,12 @@
+import Button from "../../_components/ui/Button";
+
 export default function Success() {
   return (
-    <div className="py-8">
+    <div className="py-8 px-12 max-w-7xl mx-auto">
       <h1>Success page</h1>
+      <Button variant={"Medium"} color={"Primary"} width={"Fit"} href="/">
+        Back to Home
+      </Button>
     </div>
   );
 }
