78b1f3c08a257415d70413e7ca5e8935ac136183
Helene Syre <syrehelene@gmail.com>
2026-09-25 12:04:43 +0200

    Fixed succes page setup

--- app/cart/success/page.tsx
+++ app/cart/success/page.tsx
@@ -1,12 +1,28 @@
+import { CreditCardCheck } from "lucide-react";
 import Button from "../../_components/ui/Button";
 
 export default function Success() {
   return (
     <div className="py-8 px-12 max-w-7xl mx-auto">
-      <h1>Success page</h1>
-      <Button variant={"Medium"} color={"Primary"} width={"Fit"} href="/">
-        Back to Home
-      </Button>
+      <div className="flex flex-col items-center text-center gap-8 p-14 bg-stone-50 rounded shadow-md">
+        <CreditCardCheck
+          className="bg-emerald-100 text-emerald-600 p-4 rounded-full"
+          size={72}
+          strokeWidth={1.5}
+        />
+        <div className="flex flex-col items-center gap-2">
+          <h1>Order Successful!</h1>
+          <p>
+            Thank you for your purchase. Your order has been successfully
+            placed.
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <Button variant={"Medium"} color={"Primary"} href="/">
+            Back to Home
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
