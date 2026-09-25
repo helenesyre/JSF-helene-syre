004fafa2ca4c5c9f301034f53c40b6474e6e3ec4
Helene Syre <syrehelene@gmail.com>
2026-09-25 19:14:54 +0200

    Testing button aria-label

--- app/_components/ui/Button.tsx
+++ app/_components/ui/Button.tsx
@@ -41,6 +41,7 @@ export type ButtonProps = {
   children?: ReactNode;
   className?: string;
   onClick?: () => void;
+  ariaLabel?: string;
 };
 
 export default function Button(props: ButtonProps) {
@@ -84,7 +85,12 @@ export default function Button(props: ButtonProps) {
     );
   }
   return (
-    <button type={type} className={buttonClasses} onClick={onClick}>
+    <button
+      type={type}
+      className={buttonClasses}
+      onClick={onClick}
+      aria-label={props.ariaLabel}
+    >
       {content}
     </button>
   );
