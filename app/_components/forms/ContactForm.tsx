364213b4c08a9f1d30daf61d1599985913d41ed6
Helene Syre <syrehelene@gmail.com>
2026-09-26 15:32:33 +0200

    Added new success message to contact form

--- app/_components/forms/ContactForm.tsx
+++ app/_components/forms/ContactForm.tsx
@@ -32,99 +32,108 @@ export default function ContactForm() {
     mode: "onBlur",
   });
 
-  const onSubmit: SubmitHandler<contactFormData> = (data) => {
+  const onSubmit: SubmitHandler<contactFormData> = (data, event) => {
+    event?.target.reset();
     setSubmitted(true);
   };
 
-  if (submitted) {
-    return <p>Thank you for your message!</p>;
-  }
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className={inputContainerClass}>
-        <label htmlFor="nameContact" className={labelClass}>
-          Full name<span className={asteriskClass}>*</span>
-        </label>
-        <input
-          className={inputClass}
-          id="nameContact"
-          {...register("name")}
-          aria-invalid={errors.name ? "true" : "false"}
-          aria-describedby={errors.name ? "nameError" : undefined}
-        />
-        {errors.name && (
-          <div className={errorContainerClass}>
-            <CircleAlert size={18} strokeWidth={1.75} />
-            <p id="nameError" role="alert" className={errorClass}>
-              {errors.name.message}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className={inputContainerClass}>
-        <label htmlFor="emailContact" className={labelClass}>
-          Email<span className={asteriskClass}>*</span>
-        </label>
-        <input
-          className={inputClass}
-          id="emailContact"
-          {...register("email")}
-          aria-invalid={errors.email ? "true" : "false"}
-          aria-describedby={errors.email ? "emailError" : undefined}
-        />
-        {errors.email && (
-          <div className={errorContainerClass}>
-            <CircleAlert size={18} strokeWidth={1.75} />
-            <p id="emailError" role="alert" className={errorClass}>
-              {errors.email.message}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className={inputContainerClass}>
-        <label htmlFor="subjectContact" className={labelClass}>
-          Subject<span className={asteriskClass}>*</span>
-        </label>
-        <input
-          className={inputClass}
-          id="subjectContact"
-          {...register("subject")}
-          aria-invalid={errors.subject ? "true" : "false"}
-          aria-describedby={errors.subject ? "subjectError" : undefined}
-        />
-        {errors.subject && (
-          <div className={errorContainerClass}>
-            <CircleAlert size={18} strokeWidth={1.75} />
-            <p id="subjectError" role="alert" className={errorClass}>
-              {errors.subject.message}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className={inputContainerClass}>
-        <label htmlFor="messageContact" className={labelClass}>
-          Message<span className={asteriskClass}>*</span>
-        </label>
-        <textarea
-          className={inputClass}
-          id="messageContact"
-          {...register("message")}
-          aria-invalid={errors.message ? "true" : "false"}
-          aria-describedby={errors.message ? "messageError" : undefined}
-        />
-        {errors.message && (
-          <div className={errorContainerClass}>
-            <CircleAlert size={18} strokeWidth={1.75} />
-            <p id="messageError" role="alert" className={errorClass}>
-              {errors.message.message}
-            </p>
-          </div>
-        )}
-      </div>
-      <Button variant={"Medium"} color={"Primary"} width={"Full"} type="submit">
-        Send
-      </Button>
-    </form>
+    <div>
+      {submitted && (
+        <div className="mb-4 bg-emerald-50 border border-emerald-200 text-emerald-800 font-medium rounded p-2">
+          Your message has been successfully sent!
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={inputContainerClass}>
+          <label htmlFor="nameContact" className={labelClass}>
+            Full name<span className={asteriskClass}>*</span>
+          </label>
+          <input
+            className={inputClass}
+            id="nameContact"
+            {...register("name")}
+            aria-invalid={errors.name ? "true" : "false"}
+            aria-describedby={errors.name ? "nameError" : undefined}
+          />
+          {errors.name && (
+            <div className={errorContainerClass}>
+              <CircleAlert size={18} strokeWidth={1.75} />
+              <p id="nameError" role="alert" className={errorClass}>
+                {errors.name.message}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className={inputContainerClass}>
+          <label htmlFor="emailContact" className={labelClass}>
+            Email<span className={asteriskClass}>*</span>
+          </label>
+          <input
+            className={inputClass}
+            id="emailContact"
+            {...register("email")}
+            aria-invalid={errors.email ? "true" : "false"}
+            aria-describedby={errors.email ? "emailError" : undefined}
+          />
+          {errors.email && (
+            <div className={errorContainerClass}>
+              <CircleAlert size={18} strokeWidth={1.75} />
+              <p id="emailError" role="alert" className={errorClass}>
+                {errors.email.message}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className={inputContainerClass}>
+          <label htmlFor="subjectContact" className={labelClass}>
+            Subject<span className={asteriskClass}>*</span>
+          </label>
+          <input
+            className={inputClass}
+            id="subjectContact"
+            {...register("subject")}
+            aria-invalid={errors.subject ? "true" : "false"}
+            aria-describedby={errors.subject ? "subjectError" : undefined}
+          />
+          {errors.subject && (
+            <div className={errorContainerClass}>
+              <CircleAlert size={18} strokeWidth={1.75} />
+              <p id="subjectError" role="alert" className={errorClass}>
+                {errors.subject.message}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className={inputContainerClass}>
+          <label htmlFor="messageContact" className={labelClass}>
+            Message<span className={asteriskClass}>*</span>
+          </label>
+          <textarea
+            className={inputClass}
+            id="messageContact"
+            {...register("message")}
+            aria-invalid={errors.message ? "true" : "false"}
+            aria-describedby={errors.message ? "messageError" : undefined}
+          />
+          {errors.message && (
+            <div className={errorContainerClass}>
+              <CircleAlert size={18} strokeWidth={1.75} />
+              <p id="messageError" role="alert" className={errorClass}>
+                {errors.message.message}
+              </p>
+            </div>
+          )}
+        </div>
+        <Button
+          variant={"Medium"}
+          color={"Primary"}
+          width={"Full"}
+          type="submit"
+        >
+          Send
+        </Button>
+      </form>
+    </div>
   );
 }
